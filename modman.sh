a690bfc2a3315768a503d4dc74d3c889994b24d3
nx-philip <85591425+nx-philip@users.noreply.github.com>
2021-06-09 05:18:16 +0530

    Update modman.sh

--- modman.sh
+++ modman.sh
@@ -12,7 +12,8 @@ read -p "Input URL: " uri
 # Ask the user the Client IP
 read -p "Input Client IP: " Cip
 
-
+# saves the value of the triggered modsec unique id in $rules.
 rules=( $( xzcat $auditfile | grep $domainname | grep $uri | grep ${Cip} | grep 'Pattern match' | cut -d " " -f 2 | awk -F ',' '{print $2}' | awk -F ':' '{print $2}' | sed s/\"//g) );
 
+#Finds the modsec rules to be whitelisted based on the unique id.
 for element in "${rules[@]}"; do echo "${element}"; sup_catlogs $auditfile | grep --color=auto -F "${element}" | grep --color=auto -Po ' \[id\s+\\"\K[0-9]*' | grep -v "^981176$\|^4011015$\|^4049" | sort -u; echo " " ;done
