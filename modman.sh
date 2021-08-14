64c4dbf8fb328723cd9932e6f9575902c9047a14
nx-philip <85591425+nx-philip@users.noreply.github.com>
2021-08-15 02:19:51 +0530

    Update modman.sh
    1. Input Sanitization: Domain name normalization and IP address validity checks added.
    2. Multichoice options for date to bypass validy checks for date and log file name.

--- modman.sh
+++ modman.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
 source ~/.bashrc
-#Initializes an empty variable for Ticket ID
+
 Tid=" "
 
 # Ask the user for abosulte path of the audit.log.xz
-read -p "Input abosulte path of the audit.log.xz file: " auditfile
+dateArray=($(ls /var/log/httpd/modsec_audit*.xz))
 
-# Ask the user for domain name to check in modsec_audit.log.
+PS3='Please pick the date from the following: '
+
+select date in "${dateArray[@]}"
+do
+        case $date in
+        $date)
+           echo $date
+                break
+                ;;
+        *) echo "invalid option $REPLY"
+                ;;
+        esac
+done
+
+# Ask the user for domain name to check in modsec_audit.log then normalize the domainname.
 read -p "Input domain name: " domainname
+domainname=$(sup_normalizedomain $domainname)
+
+#echo $domainname                                       #//For Debugging
+
 
 # Ask the user the URL
 read -p "Input URL: " uri
 
 # Ask the user the Client IP
-read -p "Input Client IP: " Cip
+iptest='([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
+until [[ $Cip =~ ^$iptest\.$iptest\.$iptest\.$iptest$ ]]
+do
+  read -p "Input valid Client IP: " Cip
+done
+
+
 
 # Ask the user the Ticket ID
 read -p "Input Ticket ID (optional): " Tid
 
-#echo "$auditfile $domainname $uri $Cip"                  // For Debugging
+#echo "$date $domainname $uri $Cip"                            #//For Debugging
 
-rules=( $( xzcat $auditfile | grep $domainname | grep $uri | grep ${Cip} | grep 'Pattern match' | cut -d " " -f 2 | awk -F ',' '{print $2}' | awk -F ':' '{print $2}' | sed s/\"//g) );
-#for element in "${rules[@]}"; do echo "${element}"; done // For Debugging
+# Stores the Modsec Unique IDs in an array 'rules'
+rules=( $( xzcat $date | grep $domainname | grep $uri | grep ${Cip} | grep 'Pattern match' | cut -d " " -f 2 | awk -F ',' '{print $2}' | awk -F ':' '{print $2}' | sed s/\"//g) );
 
+#for element in "${rules[@]}"; do echo "${element}"; done     #//For Debugging
+
+# Basically runs sup_modsecrules on the modsec Unique IDs from the array 'rules' and filters out duplicate ones and forbidden modsec rules and prints out the final output.
 echo " "
 echo "<IfModule mod_security2.c>
   <LocationMatch $uri >
    #Ticket: $Tid   "
-for element in "${rules[@]}"; do sup_catlogs $auditfile | grep --color=auto -F "${element}" | grep --color=auto -Po ' \[id\s+\\"\K[0-9]*' | grep -v "^981176$\|^4011015$\|^4049" | sort -u; done |  sort -u | sed 's/^/   SecRuleRemoveById /'
+for element in "${rules[@]}"; do sup_catlogs $date | grep --color=auto -F "${element}" | grep --color=auto -Po ' \[id\s+\\"\K[0-9]*' | grep -v "^981176$\|^4011015$\|^4049" | sort -u; done |  sort -u | sed 's/^/   SecRuleRemoveById /'
 echo "  </LocationMatch>
 </IfModule>"
